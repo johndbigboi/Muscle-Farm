1a8e6c23a097df0c8a7453195a9818edd3167632
john <jericcarreon@yahoo.com>
2020-07-23 17:56:11 +0100

    add cook time and decsription to addrecipe route

--- app.py
+++ app.py
@@ -23,18 +23,32 @@ def dashboard():
     return render_template('dashboard.html', recipes=mongo.db.recipes.find(), workouts=mongo.db.workouts.find())
 
 
-@app.route('/recipe')
-def recipe():
-    return render_template('recipe.html', recipes=mongo.db.recipes.find(), post=mongo.db.post_recipes.find())
+@app.route('/allrecipe/<category>')
+def allrecipe(category):
+    # if statements to display the recipes base on category name
+    if category == "Pre Workout Meal":
+        recipe = mongo.db.recipes.find({"category_name": "Pre Workout Meal"})
+    elif category == "Post Workout Meal":
+        recipe = mongo.db.recipes.find({"category_name": "Post Workout Meal"})
+    else:
+        recipe = mongo.db.recipes.find()
+
+    return render_template('allrecipe.html', recipe=recipe, category_title=category, recipes=mongo.db.recipes.find())
 
 
-@app.route('/add_recipe')
+@ app.route('/recipe/<recipe_id>')
+def get_recipe(recipe_id):
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    post = mongo.db.post_recipes.find_one({"_id": ObjectId(recipe_id)})
+    return render_template("recipe.html", recipe=recipe, post=post)
+
+
+@ app.route('/add_recipe')
 def add_recipe():
-    value = mongo.db.recipes.find({'ingredients': ""})
-    return render_template('addrecipe.html', categories=mongo.db.categories.find(), value=value)
+    return render_template('addrecipe.html', categories=mongo.db.categories.find())
 
 
-@app.route('/insert_recipe', methods=['POST'])
+@ app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
     """
     ingredients_doc = {'ingredient': request.form.getlist(
@@ -50,23 +64,27 @@ def insert_recipe():
     recipe = {
         'recipe_name': request.form.get('recipe_name'),
         'category_name': request.form.get('category_name'),
+        'description': request.form.get('description'),
         'image': request.form.get('image'),
+        'prep_time': request.form.get('prep_time'),
+        'cook_time': request.form.get('cook_time'),
         'ingredients': request.form.getlist('ingredients'),
         'instructions': request.form.getlist('instructions')
     }
-    mongo.db.recipes.insert_one(recipe)
 
-    return redirect(url_for('recipe'))
+    mongo.db.recipes.insert_one(recipe)
+    return render_template('allrecipe.html')
 
 
 @ app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
     all_categories = mongo.db.categories.find()
+    prerecipes = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
     return render_template('editrecipe.html',
-                           recipes=mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)}), categories=all_categories)  # to do a find on the categories table.
+                           recipes=prerecipes, categories=all_categories)  # to do a find on the categories table.
 
 
-@app.route('/update_recipe/<recipe_id>', methods=['POST'])
+@ app.route('/update_recipe/<recipe_id>', methods=['POST'])
 # We pass in the task ID because that's our hook into the primary key.
 def update_recipe(recipe_id):
     recipes = mongo.db.recipes
@@ -77,17 +95,20 @@ def update_recipe(recipe_id):
                    {
         'recipe_name': request.form.get('recipe_name'),
         'category_name': request.form.get('category_name'),
+        'description': request.form.get('description'),
         'image': request.form.get('image'),
+        'prep_time': request.form.get('prep_time'),
+        'cook_time': request.form.get('cook_time'),
         'ingredients': request.form.getlist('ingredients'),
-        'instructions': request.form.getlist('instructions'),
+        'instructions': request.form.getlist('instructions')
     })
-    return redirect(url_for('recipe'))
+    return render_template('allrecipe.html')
 
 
 @ app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
-    return redirect(url_for('recipe'))
+    return render_template('allrecipe.html')
 
 
 @ app.route('/workouts')
@@ -97,7 +118,7 @@ def workouts():
 
 @ app.route('/relax')
 def relax():
-    return render_template('relax.html', categories=mongo.db.categories.find(), breathe=mongo.db.breathe.find())
+    return render_template('relax.html', categories=mongo.db.categories.find(), recipe=mongo.db.recipes.find())
 
 
 if __name__ == '__main__':
